beec9e53390ceb4ff9949b07b838cd0e30addb29
Mateus <mateusc.ramos@outlook.com>
2025-02-11 18:30:46 -0300

    feat: implement TCP connection handling and task supervision in MerchantServer

--- apps/merchant_server/lib/merchant_server/application.ex
+++ apps/merchant_server/lib/merchant_server/application.ex
@@ -7,13 +7,13 @@ defmodule MerchantServer.Application do
 
   @impl true
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
     children = [
-      # Starts a worker by calling: MerchantServer.Worker.start_link(arg)
-      # {MerchantServer.Worker, arg}
+      {Task.Supervisor, name: MerchantServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> MerchantServer.accept(port) end}, restart: :permanent)
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MerchantServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
